fe45def120bf344833a038bb0f84151dc2385aea
Balathilakar <bala.thilakar@gmail.com>
2017-10-03 21:38:11 -0500

    Delete Duplicate values in the Linked Lists no buffer

---
+++ Linked Lists/DeleteDuplicateLinkedListNoBuffer.java
@@ -0,0 +1,48 @@
+package com.amfam.billing.assetsync;
+
+/*
+ * Delete Duplicate values in the LinkedList no buffer
+ */
+
+public class DeleteDuplicateLinkedListNoBuffer {	
+	
+	static class Node{
+			
+		int data;
+		Node next;
+		
+		public Node(int dt){
+			this.data = dt;
+		}
+	}
+	
+	public static void main(String[] ag){
+		
+		Node root = new Node(2);
+		root.next = new Node(5);
+		root.next.next = new Node(8);
+		root.next.next.next = new Node(2);
+		root.next.next.next.next = new Node(5);
+
+		DeleteDuplicateLLNoBuffer ddll = new DeleteDuplicateLLNoBuffer();
+		ddll.deleteDup(root);
+		System.out.print(root);
+	}
+	
+	public void deleteDup(Node head)
+	{
+		Node current = head;
+
+		while(current != null){
+				Node runner = current;
+				while(runner.next != null){
+					if(runner.next.data == current.data){
+						runner.next = runner.next.next;
+					}else{
+						runner = runner.next;
+					}
+				}
+			current = current.next;		
+			}
+		}
+}
